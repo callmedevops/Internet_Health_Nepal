7e9adf6f60f349d58542ad819a793d0ab1249202
surajsah2053 <surajsah2053@gmail.com>
2020-06-19 23:24:56 +0545

    first commit

---
+++ src/SpeedTest.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const Server = express();
+const randomBytes = require('random-bytes');
+const path = require('path');
+const request = require('request');
+const helpers = require('./Helpers');
+
+let cache = {
+    size: 0,
+    random: null
+};
+
+Server.get('/empty', function (req, res) {
+    res.sendStatus(200);
+});
+
+Server.post('/empty', function (req, res) {
+    res.set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+    res.set("Cache-Control", "post-check=0, pre-check=0");
+    res.set("Pragma", "no-cache");
+    res.sendStatus(200);
+});
+
+Server.get('/garbage', function (req, res) {
+    res.set('Content-Description', 'File Transfer');
+    res.set('Content-Type', 'application/octet-stream');
+    res.set('Content-Disposition', 'attachment; filename=random.dat');
+    res.set('Content-Transfer-Encoding', 'binary');
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+    res.set('Cache-Control', 'post-check=0, pre-check=0', false);
+    res.set('Pragma', 'no-cache');
+    const requestedSize = (req.query.ckSize || 100);
+    if (cache.size === requestedSize) {
+        res.end(cache.random);
+    } else {
+        const size = 1048576 * requestedSize;
+        randomBytes(size, (error, bytes) => {
+            if (error) res.sendStatus(500);
+            else {
+                cache.random = bytes;
+                cache.size = requestedSize;
+                res.end(bytes);
+            }
+        })
+    }
+
+});
+
+Server.get('/getIP', function (req, res) {
+    let requestIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.headers['HTTP_CLIENT_IP'] || req.headers['X-Real-IP'] || req.headers['HTTP_X_FORWARDED_FOR'];
+    if (requestIP.substr(0, 7) === "::ffff:") {
+        requestIP = requestIP.substr(7)
+    }
+    request('https://ipinfo.io/' + requestIP + '/json', function (err, body, ipData) {
+        ipData = JSON.parse(ipData);
+        if (err) res.send(requestIP);
+        else {
+            request('https://ipinfo.io/json', function (err, body, serverData) {
+                serverData = JSON.parse(serverData);
+                if (err) res.send(`${requestIP} - ${ipData.org}, ${ipData.country}`);
+                else if (ipData.loc && serverData.loc) {
+                    const d = helpers.calcDistance(ipData.loc.split(','), serverData.loc.split(','));
+                    res.send(`${requestIP} - ${ipData.org}, ${ipData.country} (${d}km)`);
+                } else {
+                    res.send(`${requestIP} - ${ipData.org}, ${ipData.country}`);
+                }
+            })
+        }
+    });
+});
+
+Server.use(express.static(path.join(__dirname, 'public_html')));
+
+Server.listen(52359, function () {
+    console.log('Speedtest Server is up and running! at http://127.0.0.1:52359/');
+});
